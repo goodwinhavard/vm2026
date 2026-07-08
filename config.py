54608ed0e2c5f31ec4338086e74679eefbfcf1a2
Håvard Goodwin <havard.goodwin@gmail.com>
2026-07-08 22:34:13 +0200

    reverse weights.

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 
 WEIGHT_HISTORICAL = 0.6   # data/training_data_fra_kvalikk_og_hist.csv
 WEIGHT_CUSTOM_MANUAL = 0.4  # data/custom_matches_manuell.csv
-WEIGHT_EXTRA_MATCHES = 0.4  # data/extra_matches.csv
+WEIGHT_EXTRA_MATCHES = 0.8  # data/extra_matches.csv
 WEIGHT_REAL_RESULTS = 1.0   # data/real_results.csv
